cf49fe04e84166b645064a26d8f16635919e875b
CanardNocturne <arcilalexandre@gmail.com>
2021-01-06 15:30:26 +0100

    Ajout de ShortestPathAlgorithm#isLine, ShortestPathAlgorithm devient static, et ajout de doc

--- src/test/java/fr/matelots/polytech/core/util/ShortestPathAlgorithmTest.java
+++ src/test/java/fr/matelots/polytech/core/util/ShortestPathAlgorithmTest.java
@@ -2,42 +2,37 @@ package fr.matelots.polytech.core.util;
 
 import fr.matelots.polytech.core.game.Board;
 import fr.matelots.polytech.core.game.Config;
-import fr.matelots.polytech.core.game.graphics.BoardDrawer;
 import fr.matelots.polytech.engine.util.Position;
 import fr.matelots.polytech.engine.util.ShortestPathAlgorithm;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * @author Alexandre Arcil
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ShortestPathAlgorithmTest {
 
-    private static ShortestPathAlgorithm algo;
     private static Board board;
-    private static BoardDrawer drawer;
 
     @BeforeAll
     public static void init() {
         board = new Board();
-        algo = new ShortestPathAlgorithm(board);
-        drawer = new BoardDrawer(board);
     }
 
-    @Test @DisplayName("La position de départ et d'arrivé est la même")
+    @Test @DisplayName("La position de départ et d'arrivé est la même") @Order(1)
     public void samePosition() {
-        List<Position> positions = algo.shortestPath(Config.POND_POSITION, Config.POND_POSITION);
+        List<Position> positions = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, Config.POND_POSITION, board);
         assertTrue(Collections.singletonList(Config.POND_POSITION).containsAll(positions));
     }
 
-    @Test @DisplayName("Les positions forment une ligne droite")
+    @Test @DisplayName("Les positions forment une ligne droite") @Order(2)
     public void linePositions() {
         Position goal = new Position(3, 0, -3);
         Position posPath1 = new Position(1, 0, -1);
@@ -48,11 +43,11 @@ public class ShortestPathAlgorithmTest {
         board.addBambooPlantation(posPath2);
         board.addBambooPlantation(new Position(3, -1, -2));
         board.addBambooPlantation(goal);
-        List<Position> positions = algo.shortestPath(Config.POND_POSITION, goal);
+        List<Position> positions = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, goal, board);
         assertTrue(Arrays.asList(Config.POND_POSITION, posPath1, posPath2, goal).containsAll(positions));
     }
 
-    @Test @DisplayName("La position a atteindre n'est pas en ligne droite")
+    @Test @DisplayName("La position a atteindre n'est pas en ligne droite") @Order(3)
     public void complexPositions() {
         Position goal = new Position(1, 2, -3);
         Position posPath1 = new Position(1, 0, -1);
@@ -62,9 +57,32 @@ public class ShortestPathAlgorithmTest {
         board.addBambooPlantation(new Position(3, 1, -4));
         board.addBambooPlantation(new Position(2, 2, -4));
         board.addBambooPlantation(goal);
-        drawer.print();
-        List<Position> positions = algo.shortestPath(Config.POND_POSITION, goal);
+        List<Position> positions = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, goal, board);
         assertTrue(Arrays.asList(Config.POND_POSITION, posPath1, posPath2, posPath3, goal).containsAll(positions));
+    }
+
+    @Test @DisplayName("La position de début et d'objectif est la même donc forme une ligne")
+    public void isLineOnePosition() {
+        List<Position> path = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, Config.POND_POSITION, board);
+        assertTrue(ShortestPathAlgorithm.isLine(path));
+    }
+
+    @Test @DisplayName("La position de début et d'objectif se trouve à côté donc forme une ligne")
+    public void isLineTwoPositions() {
+        List<Position> path = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, new Position(1, 0, -1), board);
+        assertTrue(ShortestPathAlgorithm.isLine(path));
+    }
+
+    @Test @DisplayName("La position de début et d'objectif forme une ligne")
+    public void isLine() {
+        List<Position> path = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, new Position(3, 0, -3), board);
+        assertTrue(ShortestPathAlgorithm.isLine(path));
+    }
+
+    @Test @DisplayName("La position de début et d'objectif ne forme pas une ligne")
+    public void notLine() {
+        List<Position> path = ShortestPathAlgorithm.shortestPath(Config.POND_POSITION, new Position(1, 2, -3), board);
+        assertFalse(ShortestPathAlgorithm.isLine(path));
     }
 
 }
