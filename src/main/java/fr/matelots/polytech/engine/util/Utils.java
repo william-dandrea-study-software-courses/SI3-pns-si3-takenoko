ff9d01e854bc1b6e0c467b95e4245121ea46ca98
CanardNocturne <arcilalexandre@gmail.com>
2021-01-07 11:42:40 +0100

    Ajout de getRandomElement avec un Set dans Utils

--- src/main/java/fr/matelots/polytech/engine/util/Utils.java
+++ src/main/java/fr/matelots/polytech/engine/util/Utils.java
@@ -2,7 +2,9 @@ package fr.matelots.polytech.engine.util;
 
 import fr.matelots.polytech.core.game.Config;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Alexandre Arcil
@@ -15,6 +17,13 @@ public class Utils {
     public static <E> E getRandomElement(List<E> list) {
         int index = Config.RANDOM.nextInt(list.size());
         return list.get(index);
+    }
+
+    /**
+     * Prend un élément au hasard d'un Set.
+     */
+    public static <E> E getRandomElement(Set<E> set) {
+        return getRandomElement(new ArrayList<>(set));
     }
 
 }
